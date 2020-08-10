4c52444e1c1a055857195a3b09fdb5ca5abb62a1
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-10 18:50:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
 
   @@songs = []
 
-  def initialize
+  #def initialize
     #self.class.all << self
-  end
+  #end
 
   #def self.find_by_name(name)
     #@@songs.detect{|a| a.name == name}

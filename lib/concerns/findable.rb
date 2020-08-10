e62c22c69fd2ab81d6f19fe31a06a9233e089748
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-10 19:07:05 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,14 @@
+module Findable 
+  
+  module ClassMethods
+  
+  # def find_by_name(name)
+  #   Artist.all.detect{|a| a.name == name}
+  # end
+  
+  def find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
+
+  end
+end

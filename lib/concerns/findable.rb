f3117155c9baf2666ce6e779df95522d1233594b
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-10 19:06:00 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,12 +2,12 @@ module Findable
   
   module ClassMethods
   
-  def find_by_name(name)
-    Artist.all.detect{|a| a.name == name}
-  end
+  # def find_by_name(name)
+  #   Artist.all.detect{|a| a.name == name}
+  # end
   
   def find_by_name(name)
-    Song.all.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
   end
